9f0b3c5a3468f4ae3f7a076ef7daf66153b67915
young <2461927976@qq.com>
2020-01-09 21:25:04 +0800

    详情页面增加评论，修复部分页面bug

--- test2/pages/hot/hot-detail/hot-detail.js
+++ test2/pages/hot/hot-detail/hot-detail.js
@@ -5,8 +5,7 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
@@ -20,29 +19,29 @@ Page({
   },
 
   // 点赞功能
-  fixLike:function(event) {
+  fixLike: function(event) {
     let temp = 'hotDetail.like'
     let like = this.data.hotDetail.like
     like = !like
     //向后台发送收藏数据未做
     this.setData({
-        [temp]:like
+      [temp]: like
     })
   },
 
   // 点击收藏
-  fixCollection:function(event) {
+  fixCollection: function(event) {
     let temp = 'hotDetail.collected'
     let collected = this.data.hotDetail.collected
     collected = !collected
     //向后台发送收藏数据未做
     this.setData({
-        [temp]:collected
+      [temp]: collected
     })
     wx.showToast({
-        title: collected ? "收藏成功" : "取消收藏",
-        duration: 1000,
-        icon: "success"
+      title: collected ? "收藏成功" : "取消收藏",
+      duration: 1000,
+      icon: "success"
     })
   },
 
--- test2/pages/recommend/recommend-detail/recommend-detail.js
+++ test2/pages/recommend/recommend-detail/recommend-detail.js
@@ -20,6 +20,33 @@ Page({
     })
   },
 
+  // 点赞功能
+  fixLike: function(event) {
+      let temp = 'hotDetail.like'
+      let like = this.data.hotDetail.like
+      like = !like
+      //向后台发送收藏数据未做
+      this.setData({
+          [temp]: like
+      })
+  },
+
+  // 点击收藏
+  fixCollection: function(event) {
+      let temp = 'hotDetail.collected'
+      let collected = this.data.hotDetail.collected
+      collected = !collected
+      //向后台发送收藏数据未做
+      this.setData({
+          [temp]: collected
+      })
+      wx.showToast({
+          title: collected ? "收藏成功" : "取消收藏",
+          duration: 1000,
+          icon: "success"
+      })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
