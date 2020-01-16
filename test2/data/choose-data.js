7b27394b50e3a2e828dac2d576859d7090aaf890
young <2461927976@qq.com>
2020-01-16 12:50:03 +0800

    重新实现选择爱好技术标签页面，自定义多选按钮

---
+++ test2/data/choose-data.js
@@ -0,0 +1,76 @@
+let contents = [
+    {
+        technologyId: 0,
+        name: "后端",
+        checked: false
+    },
+    {
+        technologyId: 1,
+        name: "运维",
+        checked: false
+    },
+    {
+        technologyId: 2,
+        name: "设计",
+        checked: false
+    },
+    {
+        technologyId: 3,
+        name: "算法",
+        checked: false
+    },
+    {
+        technologyId: 4,
+        name: "Android",
+        checked: false
+    },
+    {
+        technologyId: 5,
+        name: "人工智能",
+        checked: false
+    },
+    {
+        technologyId: 6,
+        name: "安全",
+        checked: false
+    },
+    {
+        technologyId: 7,
+        name: "产品/运营",
+        checked: false
+    },
+    {
+        technologyId: 8,
+        name: "大数据",
+        checked: false
+    },
+    {
+        technologyId: 9,
+        name: "ios",
+        checked: false
+    },
+    {
+        technologyId: 10,
+        name: "测试",
+        checked: false
+    },
+    {
+        technologyId: 11,
+        name: "数据库管理",
+        checked: false
+    },
+    {
+        technologyId: 12,
+        name: "前端",
+        checked: false
+    },
+    {
+        technologyId: 13,
+        name: "硬件开发",
+        checked: false
+    }
+]
+
+module.exports = {
+    techList: contents
+}
--- test2/pages/choose/choose.js
+++ test2/pages/choose/choose.js
@@ -1,129 +1,37 @@
 // pages/choose/choose.js
+let techData = require('../../data/choose-data')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    disabled: true,
-    tag1: true,
-    tag2: true,
-    tag3: true,
-    tag4: true,
-    tag5: true,
-    tag6: true,
-    tag7: true,
-    tag8: true,
-    tag9: true,
-    tag10: true,
-    tag11: true,
-    tag12: true,
-    tag13: true,
-    tag14: true
+    disabled: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.setData({
+      contents: techData.techList
+    })
+  },
+  onTagTap: function(event) {
+    let techid = event.currentTarget.dataset.techid
+    for (let i = 0; i < this.data.contents.length; i++) {
+      if (this.data.contents[i].technologyId == techid) {
+        if (this.data.contents[i].checked == true) {
+          this.data.contents[i].checked = false;
 
-  },
-  onTagTap1: function(event) {
-    let tag1 = this.data.tag1
-    tag1 = !tag1
+        } else {
+          this.data.contents[i].checked = true;
+
+        }
+      }
+    }
     this.setData({
-      tag1: tag1
-    })
-  },
-  onTagTap2: function(event) {
-    let tag2 = this.data.tag2
-    tag2 = !tag2
-    this.setData({
-      tag2: tag2
-    })
-  },
-  onTagTap3: function(event) {
-    let tag3 = this.data.tag3
-    tag3 = !tag3
-    this.setData({
-      tag3: tag3
-    })
-  },
-  onTagTap4: function(event) {
-    let tag4 = this.data.tag4
-    tag4 = !tag4
-    this.setData({
-      tag4: tag4
-    })
-  },
-  onTagTap5: function(event) {
-    let tag5 = this.data.tag5
-    tag5 = !tag5
-    this.setData({
-      tag5: tag5
-    })
-  },
-  onTagTap6: function(event) {
-    let tag6 = this.data.tag6
-    tag6 = !tag6
-    this.setData({
-      tag6: tag6
-    })
-  },
-  onTagTap7: function(event) {
-    let tag7 = this.data.tag7
-    tag7 = !tag7
-    this.setData({
-      tag7: tag7
-    })
-  },
-  onTagTap8: function(event) {
-    let tag8 = this.data.tag8
-    tag8 = !tag8
-    this.setData({
-      tag8: tag8
-    })
-  },
-  onTagTap9: function(event) {
-    let tag9 = this.data.tag9
-    tag9 = !tag9
-    this.setData({
-      tag9: tag9
-    })
-  },
-  onTagTap10: function(event) {
-    let tag10 = this.data.tag10
-    tag10 = !tag10
-    this.setData({
-      tag10: tag10
-    })
-  },
-  onTagTap11: function(event) {
-    let tag11 = this.data.tag11
-    tag11 = !tag11
-    this.setData({
-      tag11: tag11
-    })
-  },
-  onTagTap12: function(event) {
-    let tag12 = this.data.tag12
-    tag12 = !tag12
-    this.setData({
-      tag12: tag12
-    })
-  },
-  onTagTap13: function(event) {
-    let tag13 = this.data.tag13
-    tag13 = !tag13
-    this.setData({
-      tag13: tag13
-    })
-  },
-  onTagTap14: function(event) {
-    let tag14 = this.data.tag14
-    tag14 = !tag14
-    this.setData({
-      tag14: tag14
+      contents: this.data.contents
     })
   },
 
