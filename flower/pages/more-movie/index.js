5bc3f33849a3dd73084c0444b17059a266f14e43
zzj <2461927976@qq.com>
2022-02-13 13:25:45 +0800

    动态设置页面标题

--- flower/pages/more-movie/index.js
+++ flower/pages/more-movie/index.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    movies:[]
+    movies: [],
+    type: ''
   },
 
   /**
@@ -14,15 +15,14 @@ Page({
    */
   onLoad: function (options) {
     const type = options.type
-    this.getMovies(type)
+    this.data.type = type
+    this.getMovies()
   },
 
-  getMovies(type){
-    console.log(type)
+  getMovies() {
     wx.request({
-      url: `${app.globalData.baseUrl}${type}?start=0&count=12`,
+      url: `${app.globalData.baseUrl}${this.data.type}?start=0&count=12`,
       success: res => {
-        console.log(res)
         this.setData({
           movies: res.data.subjects
         })
@@ -34,7 +34,23 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    let title = ""
+    switch (this.data.type) {
+      case "in_theaters":
+        title = "正在热映"
+        break;
+      case "coming_soon":
+        title = "即将上映"
+        break;
+      case "top250":
+        title = "豆瓣Top250"
+        break;
+      default:
+        break;
+    }
+    wx.setNavigationBarTitle({
+      title,
+    })
   },
 
   /**
@@ -62,14 +78,31 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.request({
+      url: `${app.globalData.baseUrl}${this.data.type}?start=0&count=12`,
+      success: res => {
+        this.setData({
+          movies: res.data.subjects
+        })
+        wx.stopPullDownRefresh()
+      }
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    wx.showNavigationBarLoading()
+    wx.request({
+      url: `${app.globalData.baseUrl}${this.data.type}?start=${this.data.movies.length}&count=12`,
+      success: res => {
+        this.setData({
+          movies: this.data.movies.concat(res.data.subjects)
+        })
+        wx.hideNavigationBarLoading()
+      }
+    })
   },
 
   /**
--- flower/pages/movies/index.js
+++ flower/pages/movies/index.js
@@ -91,7 +91,6 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
   },
 
   /**
